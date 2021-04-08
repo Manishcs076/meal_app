526f2f0a08d16b687f086d611a05131ce20cfdb1
manish123 <manishcs036@gmail.com>
2021-04-08 11:22:19 +0530

    using pushNamed instead of push in Navigator and passing arguments

--- lib/category_item.dart
+++ lib/category_item.dart
@@ -1,18 +1,15 @@
 import 'package:flutter/material.dart';
-import './category_meals_screen.dart';
 
 class CategoryItem extends StatelessWidget {
   final String title;
   final Color color;
-  CategoryItem(this.title, this.color);
+  final String id;
+  CategoryItem(this.title, this.color, this.id);
 
   void selectCategory(BuildContext ctx) {
-    Navigator.of(ctx).push(
-      MaterialPageRoute(
-        builder: (_) {
-          return CategoryMealsScreen();
-        },
-      ),
+    Navigator.of(ctx).pushNamed(
+      '/categories-meals',
+      arguments: {'id': id, 'title': title},
     );
   }
 
