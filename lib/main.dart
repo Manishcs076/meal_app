db0d3844e122b48651ee63d0246f24e46dc8df0a
manish123 <manishcs036@gmail.com>
2021-04-10 14:19:28 +0530

    return MaaterialPageRoute

--- lib/main.dart
+++ lib/main.dart
@@ -40,7 +40,9 @@ class MyApp extends StatelessWidget {
       },
       onGenerateRoute: (settings) {
         print(settings.arguments);
-
+        return MaterialPageRoute(
+          builder: (ctx) => CategoriesScreen(),
+        );
       },
       onUnknownRoute: (settings) {
         return MaterialPageRoute(
