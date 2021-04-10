7cb9bee1ef1bafc8eb15d5787de9a21fefa5e2c1
manish123 <manishcs036@gmail.com>
2021-04-10 14:14:36 +0530

    adding  selectedMeal by meal id

--- lib/screens/meal_detail_screen.dart
+++ lib/screens/meal_detail_screen.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
+import '../dummy_data.dart';
 
 class MealDetailScreen extends StatelessWidget {
   static const routeName = '/meal-detail';
   @override
   Widget build(BuildContext context) {
     final mealId = ModalRoute.of(context).settings.arguments as String;
+    final selectedMeal = DUMMY_MEALS.firstWhere((meal) => meal.id == mealId);
     return Scaffold(
       appBar: AppBar(
         title: Text('$mealId'),
       ),
-      body: Center(
-        child: Text('Details of meal -$mealId'),
+      body: Column(
+        children: [
+          Container(
+            height: 300,
+            width: double.infinity,
+            child: Image.network(selectedMeal.imageUrl,fit: BoxFit.cover,),
+          ),
+        ],
       ),
     );
   }
